42a8019dec18b13a74ae85a18edc31bf7a796a53
jaaaco <jakub@wietrzyk.com>
2017-09-15 12:59:14 +0200

    update to meteor 1.5.2

--- server/main.js
+++ server/main.js
@@ -6,8 +6,8 @@ ServiceConfiguration.configurations.remove({
 
 ServiceConfiguration.configurations.insert({
     service: "facebook",
-    appId: 'YOUR_APP_ID',
-    secret: 'YOUR_APP_SECRET'
+    appId: '1719858061604657',
+    secret: '2e0d73892c36fda9512e66897049c209'
 });
 
 Accounts.onCreateUser(function (options, user) {
